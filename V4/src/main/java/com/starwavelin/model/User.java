72ae552d7830f5dc35c158a10f3bfb147f48c20c
Benjamin X. Lin <benjaminxianlin@gmail.com>
2015-07-19 15:32:25 -0400

    V4: add the POJOs for User (Admin), Customer and Vehicle.

---
+++ V4/src/main/java/com/starwavelin/model/User.java
@@ -0,0 +1,28 @@
+package com.starwavelin.model;
+
+public class User {
+	private String username;
+	private String password;
+	private boolean flag;
+	
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public void setValid(boolean flag) {
+		this.flag = flag;
+	}
+	public boolean isValid() {
+		return flag;
+	}
+}
---
+++ V4/src/main/java/com/starwavelin/model/Vehicle.java
@@ -0,0 +1,53 @@
+package com.starwavelin.model;
+
+public class Vehicle {
+	private String vin, make, model, type, picture;
+	private int year, cusid;	// cusid is the one associated with customerID
+	private double amount;
+	
+	public Vehicle() {}
+	
+	
+	// Accessor methods
+	public String getVin() {return vin;}
+	public String getMake() {return make;}
+	public String getModel() {return model;}
+	public String getType() {return type;}
+	public int getYear() {return year;}
+	public String getPicture() {return picture;}	
+	public double getAmount() {return amount;}
+	public int getCusid() {return cusid;}
+	
+	// Mutator methods
+	public void setVin(String vin) {
+		this.vin = vin;
+	}
+	
+	public void setMake(String make) {
+		this.make = make;
+	}
+	
+	public void setModel(String model) {
+		this.model = model;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+	
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	
+	public void setCusid(int cusid) {
+		this.cusid = cusid;
+	}
+}
